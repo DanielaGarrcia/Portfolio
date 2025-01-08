3db366c0e943a64d0222e5097d9b57ecb876316c
DanielaGarrcia <2120100453@soy.utj.edu.mx>
2025-01-07 19:53:14 -0600

    Repositorio iniciado

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './styles/App.css';
+
+import Home from './components/Home'
+import Experience from './components/Experience'
+import Portfolio from './components/Portfolio'
+import Skills from './components/Skills'
+
+function App() {
+  return (
+    <>
+      <Home />
+      <Experience />
+      <Portfolio />
+      <Skills />
+    </>
+  );
+}
+
+export default App;
